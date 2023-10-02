12221a9123506bd3805a3020c3d699c8447df52c
Xzyaihni <Fxlictx996@gmail.com>
2023-10-02 21:43:40 +0800

    separated recursive file loading into its own thingy

--- src/assets.rs
+++ src/assets.rs
@@ -26,138 +26,47 @@ pub enum DefaultModel
     Square
 }
 
-pub struct Assets
+pub struct NamedValue<T>
 {
-	texture_ids: HashMap<String, usize>,
-	textures: Vec<Arc<RwLock<Texture>>>,
-	model_ids: HashMap<String, usize>,
-	models: Vec<Arc<RwLock<Model>>>,
-    default_models: Vec<Arc<RwLock<Model>>>
+    pub name: String,
+    pub value: T
 }
 
-impl Assets
+impl<T> NamedValue<T>
 {
-    pub fn new<TexturesPath, ModelsPath>(
-        resource_uploader: &mut ResourceUploader,
-        textures_path: Option<TexturesPath>,
-        models_path: Option<ModelsPath>
-    ) -> Self
+    pub fn map<U, F>(self, f: F) -> NamedValue<U>
     where
-        TexturesPath: AsRef<Path>,
-        ModelsPath: AsRef<Path>
+        F: FnOnce(T) -> U
     {
-        let (textures, texture_ids) = Self::load_named_assets(textures_path, |path|
-        {
-			let image = RgbaImage::load(path).unwrap();
+        let NamedValue{
+            name,
+            value
+        } = self;
 
-            Texture::new(resource_uploader, image)
-        });
-
-        let (models, model_ids) = Self::load_named_assets(models_path, |path|
-        {
-            Model::load(path).unwrap()
-        });
-
-        let default_models = Self::create_default_models();
-
-        Self{texture_ids, textures, model_ids, models, default_models}
-    }
-
-    pub fn default_model(&self, id: DefaultModel) -> Arc<RwLock<Model>>
-    {
-        self.default_models[id as usize].clone()
-    }
-
-    pub fn texture(&self, name: &str) -> Arc<RwLock<Texture>>
-    {
-        self.textures[self.texture_ids[name]].clone()
-    }
-
-    pub fn model(&self, name: &str) -> Arc<RwLock<Model>>
-    {
-        self.models[self.model_ids[name]].clone()
-    }
-
-    pub fn add_textures<T>(&mut self, textures: T)
-    where
-        T: IntoIterator<Item=(String, Texture)>
-    {
-        Self::add_assets(textures.into_iter(), &mut self.textures, &mut self.texture_ids);
-    }
-
-    pub fn add_models<T>(&mut self, models: T)
-    where
-        T: IntoIterator<Item=(String, Model)>
-    {
-        Self::add_assets(models.into_iter(), &mut self.models, &mut self.model_ids);
-    }
-
-    fn create_default_models() -> Vec<Arc<RwLock<Model>>>
-    {
-        DefaultModel::iter().map(|default_model|
-        {
-            let model;
-
-            match default_model
-            {
-                DefaultModel::Square =>
-                {
-                    model = Model::square(1.0);
-                }
-            }
-
-            Arc::new(RwLock::new(model))
-        }).collect()
-    }
-
-    fn add_assets<AddAssetsType, T>(
-        add_assets: AddAssetsType,
-        assets: &mut Vec<Arc<RwLock<T>>>,
-        asset_ids: &mut HashMap<String, usize>
-    )
-    where
-        AddAssetsType: Iterator<Item=(String, T)>
-    {
-        let index_offset = assets.len();
-        let (insert_assets, map_assets): (Vec<_>, Vec<(_, usize)>) =
-            add_assets.enumerate().map(|(index, (name, asset))|
-            {
-                (Arc::new(RwLock::new(asset)), (name, index_offset + index))
-            }).unzip();
-
-        assets.extend(insert_assets);
-        asset_ids.extend(map_assets);
-    }
-
-	pub fn swap_pipeline(&mut self, info: &PipelineInfo)
-	{
-		self.textures.iter_mut().for_each(|texture|
-		{
-			texture.write().swap_pipeline(info)
-		});
-	}
-
-    fn load_named_assets<P, F, T>(
-        folder_path: Option<P>,
-        mut loader: F
-    ) -> (Vec<Arc<RwLock<T>>>, HashMap<String, usize>)
-    where
-        P: AsRef<Path>,
-        F: FnMut(PathBuf) -> T
-    {
-        if folder_path.is_none()
-        {
-            let assets = Vec::new();
-            let asset_indices = HashMap::new();
-
-            return (assets, asset_indices);
+        NamedValue{
+            name,
+            value: f(value)
         }
+    }
+}
 
-        let folder_path = folder_path.unwrap();
+pub struct FilesLoader;
 
-		let assets = Self::recursive_dir(folder_path.as_ref()).into_iter().map(|name|
+impl FilesLoader
+{
+    pub fn load_images(folder_path: impl AsRef<Path>) -> impl Iterator<Item=NamedValue<RgbaImage>>
+    {
+        Self::load(folder_path).map(|named_value|
+        {
+            named_value.map(|path| RgbaImage::load(path).unwrap())
+        })
+    }
+
+    pub fn load(folder_path: impl AsRef<Path>) -> impl Iterator<Item=NamedValue<PathBuf>>
+    {
+		Self::recursive_dir(folder_path.as_ref()).into_iter().map(|name|
 		{
-            let value = Arc::new(RwLock::new(loader(name.clone())));
+            let value = name.clone();
 
 			let short_path = name.iter().skip(1).fold(PathBuf::new(), |mut acc, part|
 			{
@@ -166,18 +75,8 @@ impl Assets
 				acc
 			}).into_os_string().into_string().unwrap();
 
-			(short_path, value)
-		}).collect::<HashMap<_, _>>();
-
-        let (indices_map, data) = assets.into_iter().enumerate().map(|(index, (name, asset))|
-        {
-            let named_index = (name, index);
-            let value = asset;
-
-            (named_index, value)
-        }).unzip();
-
-        (data, indices_map)
+            NamedValue{name: short_path, value}
+		})
     }
 
 	fn recursive_dir(path: &Path) -> impl Iterator<Item=PathBuf>
@@ -202,5 +101,130 @@ impl Assets
 				collector.push(entry.path());
 			}
 		})
+	}
+}
+
+pub struct Assets
+{
+	textures: HashMap<String, Arc<RwLock<Texture>>>,
+	models: HashMap<String, Arc<RwLock<Model>>>,
+    default_models: Vec<Arc<RwLock<Model>>>
+}
+
+impl Assets
+{
+    pub fn new<TexturesPath, ModelsPath>(
+        resource_uploader: &mut ResourceUploader,
+        textures_path: Option<TexturesPath>,
+        models_path: Option<ModelsPath>
+    ) -> Self
+    where
+        TexturesPath: AsRef<Path>,
+        ModelsPath: AsRef<Path>
+    {
+        let textures = Self::load_resource(textures_path, |path|
+        {
+            FilesLoader::load_images(path).map(|named_value|
+            {
+                named_value.map(|image| Texture::new(resource_uploader, image))
+            })
+        });
+
+        let models = Self::load_resource(models_path, |path|
+        {
+            FilesLoader::load(path).map(|named_value|
+            {
+                named_value.map(|path| Model::load(path).unwrap())
+            })
+        });
+
+        let default_models = Self::create_default_models();
+
+        Self{textures, models, default_models}
+    }
+
+    fn load_resource<T, F, I, P>(maybe_path: Option<P>, f: F) -> HashMap<String, Arc<RwLock<T>>>
+    where
+        P: AsRef<Path>,
+        I: Iterator<Item=NamedValue<T>>,
+        F: FnOnce(P) -> I
+    {
+        maybe_path.map(|path|
+        {
+            f(path).map(|NamedValue{name, value}|
+            {
+                (name, Arc::new(RwLock::new(value)))
+            }).collect()
+        }).unwrap_or_else(|| HashMap::new())
+    }
+
+    pub fn default_model(&self, id: DefaultModel) -> Arc<RwLock<Model>>
+    {
+        self.default_models[id as usize].clone()
+    }
+
+    pub fn texture(&self, name: &str) -> Arc<RwLock<Texture>>
+    {
+        self.textures[name].clone()
+    }
+
+    pub fn model(&self, name: &str) -> Arc<RwLock<Model>>
+    {
+        self.models[name].clone()
+    }
+
+    pub fn add_textures<T>(&mut self, textures: T)
+    where
+        T: IntoIterator<Item=(String, Texture)>
+    {
+        Self::add_assets(textures.into_iter(), &mut self.textures);
+    }
+
+    pub fn add_models<T>(&mut self, models: T)
+    where
+        T: IntoIterator<Item=(String, Model)>
+    {
+        Self::add_assets(models.into_iter(), &mut self.models);
+    }
+
+    fn create_default_models() -> Vec<Arc<RwLock<Model>>>
+    {
+        DefaultModel::iter().map(|default_model|
+        {
+            let model;
+
+            match default_model
+            {
+                DefaultModel::Square =>
+                {
+                    model = Model::square(1.0);
+                }
+            }
+
+            Arc::new(RwLock::new(model))
+        }).collect()
+    }
+
+    fn add_assets<AddAssetsType, T>(
+        add_assets: AddAssetsType,
+        assets: &mut HashMap<String, Arc<RwLock<T>>>
+    )
+    where
+        AddAssetsType: Iterator<Item=(String, T)>
+    {
+        let insert_assets = add_assets.map(|(name, asset)|
+        {
+            (name, Arc::new(RwLock::new(asset)))
+        }).collect::<Vec<_>>();
+
+        assets.extend(insert_assets);
+    }
+
+	pub fn swap_pipeline(&mut self, info: &PipelineInfo)
+	{
+		self.textures.iter_mut().for_each(|(_name, texture)|
+		{
+			texture.write().swap_pipeline(info)
+		});
 	}
 }
